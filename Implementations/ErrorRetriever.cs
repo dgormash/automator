a4ff3b5471c71625937d376ede154d9276d6c2b3
D. Gormash <dlgormash@gmail.com>
2017-06-23 16:34:19 +0500

    Веселье продолжается!!!

---
+++ Implementations/ErrorRetriever.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using AutomatorPrg.Interfaces;
+
+namespace AutomatorPrg.Implementations
+{
+    class ErrorRetriever:IErrorRetriever
+    {
+        public Dictionary<string, string> GetAllErrors(string file)
+        {
+            var result = new Dictionary<string, string>();
+            var lines = File.ReadAllLines(file);
+            var lineNum = string.Empty;
+            var atributes = string.Empty;
+            foreach (var line in lines)
+            {
+                foreach (Match m in Regex.Matches(line, @"(^[0-9]+?):"))
+                {
+                    lineNum = m.Groups[1].Value; 
+                }
+
+                foreach (Match m in Regex.Matches(line, @"/([0-9]{2}(.+?))$"))
+                {
+                    atributes = m.Groups[1].Value;
+                }
+
+                result.Add(lineNum, atributes);
+            }
+            return result;
+        }
+    }
+}
---
+++ Interfaces/IErrorRetriever.cs
@@ -0,0 +1,7 @@
+﻿namespace AutomatorPrg.Interfaces
+{
+    public interface IErrorRetriever
+    {
+         
+    }
+}
